afec9933a0f0916336b9f3f69e43c32e17aa3dd3
enriquegarcia96 <enriquegarcia259@yahoo.es>
2021-04-28 08:50:52 -0600

    iniciando proyecto

---
+++ index.js
@@ -0,0 +1,10 @@
+const { ApolloServer, gql }= require('apollo-server');
+
+
+
+const server = new ApolloServer();
+
+
+server.listen().then( ({url}) => {
+    console.log(`Servidor listo en la URL ${url}`);
+} )
