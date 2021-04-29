8e4a8f7feb4701e41617932fe881c03e5d7aab8a
enriquegarcia96 <enriquegarcia259@yahoo.es>
2021-04-28 22:14:45 -0600

    mutation para los inputs y sus valores de retorno

--- db/schema.js
+++ db/schema.js
@@ -5,6 +5,8 @@ const {  gql } = require('apollo-server');
 //--- type definition ---//
 const typeDefs = gql`
 
+    # lo que retorna
+
     type Token {
         token: String
     }
@@ -15,8 +17,28 @@ const typeDefs = gql`
         id: ID
     }
 
+    type Tarea {
+        nombre: String
+        id: ID
+        proyecto: String
+        estado: Boolean
+    }
+
+
+
+
     type Query{
         obtenerProyectos : [Proyecto]
+
+        obtenerTareas(input: ProyectoIDInput) : [Tarea]
+    }
+
+
+
+    # los input(entradas)
+
+    input ProyectoIDInput {
+        proyecto: String!
     }
 
     input UsuarioInput {
@@ -34,14 +56,31 @@ const typeDefs = gql`
         nombre: String!
     }
 
+    input TareaInput {
+        nombre: String!
+        proyecto: String!
+    }
+
+   
+
+
 
    
     type Mutation {
+
+        # Proyectos
         crearUsuario(input: UsuarioInput) : String
         autenticarUsuario(input: AutenticarInput) : Token
         nuevoProyecto(input: ProyectoInput) : Proyecto
         actualizarProyecto(id : ID!, input: ProyectoInput) : Proyecto
         eliminarProyecto(id: ID!) : String
+
+
+
+        # Tareas
+        nuevaTarea(input: TareaInput) : Tarea
+        actualizarTarea(id: ID!, input: TareaInput, estado: Boolean) : Tarea
+        eliminarTarea(id: ID!) : String
     }
 
 `;
